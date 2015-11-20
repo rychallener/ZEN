d13f7238a76e7354494258d6da5df0909c35acec
Ryan Challener <rchallen@knights.ucf.edu>
2015-11-20 15:52:25 -0500

    Adaptations for use with MCcubed

--- zen.py
+++ zen.py
@@ -1,14 +1,19 @@
 #em 2 nov 2015
 
+import sys
 import numpy as np
 import zen_funcs as zf
 import matplotlib.pyplot as plt
+sys.path.appen("./MCcubed/src/")
 import mccubed as mc3
 
 
 #somehow get data here, either read in something or unpack the poet event
 #also a config file of the initial parameter values or whatever for mcmc
 
-phat, dP = zf.zen_init(values)
+phat, dP = zf.zen_init(data, pixels)
 
-#call mc3.mcmc with zen as the function
+# FINDME: This is the general structure we need for MC3, but names/numbers are subject to change
+allp, bp = mc3.mcmc(data, uncert, func=zf.zen, indparams=time,
+                    params=params, numit=1e5, burnin=500)
+
--- zen_funcs.py
+++ zen_funcs.py
@@ -7,11 +7,13 @@ def zen_init(data, pixels):
 	"""
     This function does the initial calculations for pixel-level decorrelation.
 
-    Arguments
-    ---------
-	data: 3D float array of images
+    Parameters:
+    -----------
+	data: ndarray
+      3D float array of images
 
-    pixels: 2D array coordinates of pixels to consider
+    pixels: ndarray
+      2D array coordinates of pixels to consider
       EX: array([[ 0,  1],
                  [ 2,  3],
                  [ 4,  5],
@@ -22,6 +24,21 @@ def zen_init(data, pixels):
                  [14, 15],
                  [16, 17],
                  [18, 19]])
+
+    Returns:
+    --------
+
+    Example:
+    -------
+    >>> import numpy as np
+    >>> data = np.arange(200).reshape(8,5,5)
+    >>> pixels = [[1,2],[2,1],[2,2],[2,3],[3,2]]
+    >>> res = zen_init(data,pixels)
+
+    Modification History:
+    ---------------------
+    2015-11-02 em       Initial implementation
+    2015-11-20 rchallen Generalized for any given pixels
 
 	"""
 	# Set number of frames and image dimensions
@@ -79,57 +96,104 @@ def eclipse(times, period, t0, i, r, p, phase_m, depth):
 	"""
 	#secondary eclipse
 	
-    	P_ecl	= np.zeros(np.size(times))
+    P_ecl	= np.zeros(np.size(times))
 
 	phase	= ((times - t0) % period) / period
 
-    	r	= a * np.sqrt(1-(np.sin(i)**2) * (np.cos((phase-phase_m) * 2 * np.pi)**2))
+    r	= a * np.sqrt(1-(np.sin(i)**2) * (np.cos((phase-phase_m) * 2 * np.pi)**2))
 
-    	in_ecl	= np.where((r < 1-p) & (phase > .1) & (phase < .9))
-    	P_ecl[in_ecl] = 1
+    in_ecl	= np.where((r < 1-p) & (phase > .1) & (phase < .9))
+    P_ecl[in_ecl] = 1
 
-    	#ingress and egress
-    	ing_egr	= np.where(  (r > 1-p) & (r < 1+p) & (phase>.1) & (phase < .9))
+    #ingress and egress
+    ing_egr	= np.where(  (r > 1-p) & (r < 1+p) & (phase>.1) & (phase < .9))
 
-    	t1=np.arccos((1+r[ing_egr]**2-p**2)/(2*r[ing_egr]))
-    	t2=np.arccos((r[ing_egr]**2+p**2-1)/(2*r[ing_egr]*p))
-    	P_ecl[ing_egr]=(1/(np.pi*p**2))*(t1-np.sin(t1)*np.cos(t1))+(1/np.pi)*(t2-np.sin(t2)*np.cos(t2))
+    t1=np.arccos((1+r[ing_egr]**2-p**2)/(2*r[ing_egr]))
+    t2=np.arccos((r[ing_egr]**2+p**2-1)/(2*r[ing_egr]*p))
+    P_ecl[ing_egr]=(1/(np.pi*p**2))*(t1-np.sin(t1)*np.cos(t1))+(1/np.pi)*(t2-np.sin(t2)*np.cos(t2))
 
-    	F_ecl=depth*(1-P_ecl)
+    F_ecl=depth*(1-P_ecl)
 
 	return(F_ecl)
 
-def zen(phat, pld_params, frame_times, eclipse_params, linramp, quadramp, offset):
+# def zen(phat, pld_params, frame_times, eclipse_params, linramp, quadramp, offset):
 
-	"""
-	phat: 2d array
-		normalized pixel values in each frame 
-		or should we use pbar?
-	pld_params: 1d array
-		first derivative of signal wrt each pixel, fitted parameter	
-	eclipse_params: 1D array
-		go into eclipse/phase curve model to tell what eclipse is
-	ramp: number?
-		linear ramp term
-	quadramp: number?
-		quadratic ramp term
-	offset: number?
-		constant offset	
-	"""
-	N, times= np.shape(phat)
+# 	"""
+# 	phat: 2d array
+# 		normalized pixel values in each frame 
+# 		or should we use pbar?
+# 	pld_params: 1d array
+# 		first derivative of signal wrt each pixel, fitted parameter	
+# 	eclipse_params: 1D array
+# 		go into eclipse/phase curve model to tell what eclipse is
+# 	ramp: number?
+# 		linear ramp term
+# 	quadramp: number?
+# 		quadratic ramp term
+# 	offset: number?
+# 		constant offset	
+# 	"""
+# 	N, times= np.shape(phat)
 	
-	dS	= np.zeros(times)
+# 	dS	= np.zeros(times)
 
-	eclipse = eclipse(frame_times,*eclipse_params)
+# 	eclipse = eclipse(frame_times,*eclipse_params)
 	
-	for t in range(times):
-		for i in range(N):
-			dS[t]	= dS[t] + pld_params[i] * phat[t]
+# 	for t in range(times):
+# 		for i in range(N):
+# 			dS[t]	= dS[t] + pld_params[i] * phat[t]
 		
-		dS[t]	= dS[t] + eclipse[t] + linramp * t + quadramp * t**2 + offset
+# 		dS[t]	= dS[t] + eclipse[t] + linramp * t + quadramp * t**2 + offset
 
 
-	return(dS)
+# 	return(dS)
+
+def zen(par, x, npix):
+    """
+    Zen function.
+
+    Parameters:
+    -----------
+    par: ndarray
+      Zen parameters, eclipse parameters, ramp parameters
+    x: ndarray
+      Locations to evaluate eclipse model
+    npix: int
+      
+
+    Returns:
+    --------
+    y: ndarray
+      Model evaluated at x
+
+    Modification History:
+    ---------------------
+    2015-11-02 em       Initial implementation.
+    2015-11-20 rchallen Adaptation for use with MCcubed
+
+    Notes:
+    ------
+    Only allows for quadratic ramp functions
+    """
+
+    # PLD term in the model
+    PLD = 0
+
+    # Calculate eclipse model using parameters
+    # FINDME: remove hard-coded number of ramp params
+    eclipsepars = par[npix+1:-3]
+    
+    eclipse = eclipse(x, *eclipsepars)
+    
+    # Calculate the sum of the flux from all considered pixels
+    for i in range(npix):
+        PLD += par[i]*phat[:,i]
+
+    # Calculate the model
+    # FINDME: allow for general ramp function
+    y = PLD + eclipse + par[i+1]*x + par[i+2]*x**2 + par[i+3]
+
+    return y
 
 
 
